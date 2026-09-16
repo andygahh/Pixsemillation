5211483363e25417dd74d97d457ca030748aa859
Andrew Rogers <andygahh@gmail.com>
2026-09-16 16:20:48 -0400

    refactor: reorganize scripts and clean repository

--- Assets/Scripts/Grid/GridPhysics.cs
+++ Assets/Scripts/Grid/GridPhysics.cs
@@ -3,14 +3,7 @@ using UnityEngine;
 
 public static class GridPhysics
 {
-    #region Rotation Physics
-
-    public static int CalculateRotationStrength(Vector2Int bodypixelposition)
-    {
-        float distance = Mathf.Sqrt(Mathf.Pow(bodypixelposition.x, 2) + Mathf.Pow(bodypixelposition.y, 2));
-
-        return Mathf.RoundToInt(distance);
-    }
+    #region Rotation Actions
 
     public static void Slap(RotationHit hit, List<WorldCluster> worldClusters)
     {
@@ -64,62 +57,25 @@ public static class GridPhysics
         }
     }
 
-    public static void DebugRotationStrength()
+    #endregion
+    
+    #region Rotation Calculations
+    public static int CalculateRotationStrength(Vector2Int bodypixelposition)
     {
-        Debug.Log(CalculateRotationStrength(new Vector2Int(3,4)));
-        Debug.Log(CalculateRotationStrength(new Vector2Int(5,0)));
-        Debug.Log(CalculateRotationStrength(new Vector2Int(1,1)));
+        float distance = Mathf.Sqrt(Mathf.Pow(bodypixelposition.x, 2) + Mathf.Pow(bodypixelposition.y, 2));
+
+        return Mathf.RoundToInt(distance);
     }
 
-    #endregion
-
-    #region Positioning
-
-    public static bool IsClusterContact(Vector2Int oldPosition, Vector2Int newPosition, Vector2Int ClusterPosition, Vector2Int playerCorePosition)
+    public static Vector2Int CalculatePushDirection(MovedPixel movedPixel)
     {
-        Vector2Int oldRelativePosition = oldPosition - playerCorePosition;
-        Vector2Int newRelativePosition = newPosition - playerCorePosition;
-        Vector2Int expectedClockwise = new Vector2Int(oldRelativePosition.y, -oldRelativePosition.x);
+        Vector2Int direction = new Vector2Int();
+        Vector2Int delta = movedPixel.newPosition - movedPixel.oldPosition;
 
-        float radius = Mathf.Sqrt(Mathf.Pow(oldRelativePosition.x, 2) + Mathf.Pow(oldRelativePosition.y, 2));
-        float startAngle = Mathf.Atan2(oldRelativePosition.y, oldRelativePosition.x);
+        direction.x = System.Math.Sign(delta.x);
+        direction.y = System.Math.Sign(delta.y);
 
-        int rotationDirection;
-
-        if (newRelativePosition == expectedClockwise)
-        {
-            rotationDirection = -1;
-        }
-        else
-        {
-            rotationDirection = 1;
-        }
-
-        if (newPosition == ClusterPosition)
-        {
-            return true;
-        }
-
-        int steps = Mathf.Max(
-            Mathf.Abs(newPosition.x - oldPosition.x),
-            Mathf.Abs(newPosition.y - oldPosition.y)
-        );
-
-        for (int step = 1; step < steps; step++)
-        {
-            float t = step / (float)steps;
-            float angleOffset = (Mathf.PI / 2) * t;
-            float sampleAngle = startAngle + rotationDirection * angleOffset;
-
-            Vector2Int sampleCell = GridMath.GetGridPointOnCircle(sampleAngle, radius, playerCorePosition);
-
-            if (sampleCell == ClusterPosition)
-            {
-                return true;
-            }
-        }
-
-        return false;
+        return direction;
     }
 
     public static List<MovedPixel> FindMovedPixels(List<Vector2Int> oldPositions,List<Vector2Int> newPositions)
@@ -145,26 +101,9 @@ public static class GridPhysics
         return movedPixels;
     }
 
-    public static MovedPixel FindStrikingPixel(List<Vector2Int> oldBodyPositions, List<Vector2Int> newBodyPositions, Vector2Int playerCore, List<Vector2Int> clusterWorldPositions)
-    {
-        List<MovedPixel> movedPixels = FindMovedPixels(oldBodyPositions, newBodyPositions);
+    #endregion
 
-        foreach (MovedPixel movedPixel in movedPixels)
-        {
-            Vector2Int oldWorldPosition = playerCore + movedPixel.oldPosition;
-            Vector2Int newWorldPosition = playerCore + movedPixel.newPosition;
-
-            foreach (Vector2Int clusterPosition in clusterWorldPositions)
-            {
-                if (IsClusterContact(oldWorldPosition, newWorldPosition, clusterPosition, playerCore))
-                {
-                    return movedPixel;
-                }
-            }
-        }
-
-        return null;
-    }
+    #region Rotation Collision Detection
 
     public static RotationHit FindRotationHit(List<Vector2Int> oldBodyPositions, List<Vector2Int> newBodyPositions, Vector2Int playerCore, List<WorldCluster> worldClusters)
     {
@@ -194,15 +133,81 @@ public static class GridPhysics
         return null;
     }
 
-    public static Vector2Int CalculatePushDirection(MovedPixel movedPixel)
+    public static MovedPixel FindStrikingPixel(List<Vector2Int> oldBodyPositions, List<Vector2Int> newBodyPositions, Vector2Int playerCore, List<Vector2Int> clusterWorldPositions)
     {
-        Vector2Int direction = new Vector2Int();
-        Vector2Int delta = movedPixel.newPosition - movedPixel.oldPosition;
+        List<MovedPixel> movedPixels = FindMovedPixels(oldBodyPositions, newBodyPositions);
 
-        direction.x = System.Math.Sign(delta.x);
-        direction.y = System.Math.Sign(delta.y);
+        foreach (MovedPixel movedPixel in movedPixels)
+        {
+            Vector2Int oldWorldPosition = playerCore + movedPixel.oldPosition;
+            Vector2Int newWorldPosition = playerCore + movedPixel.newPosition;
 
-        return direction;
+            foreach (Vector2Int clusterPosition in clusterWorldPositions)
+            {
+                if (IsClusterContact(oldWorldPosition, newWorldPosition, clusterPosition, playerCore))
+                {
+                    return movedPixel;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static bool IsClusterContact(Vector2Int oldPosition, Vector2Int newPosition, Vector2Int clusterPosition, Vector2Int playerCorePosition)
+    {
+        Vector2Int oldRelativePosition = oldPosition - playerCorePosition;
+        Vector2Int newRelativePosition = newPosition - playerCorePosition;
+        Vector2Int expectedClockwise = new Vector2Int(oldRelativePosition.y, -oldRelativePosition.x);
+
+        float radius = Mathf.Sqrt(Mathf.Pow(oldRelativePosition.x, 2) + Mathf.Pow(oldRelativePosition.y, 2));
+        float startAngle = Mathf.Atan2(oldRelativePosition.y, oldRelativePosition.x);
+
+        int rotationDirection;
+
+        if (newRelativePosition == expectedClockwise)
+        {
+            rotationDirection = -1;
+        }
+        else
+        {
+            rotationDirection = 1;
+        }
+
+        if (newPosition == clusterPosition)
+        {
+            return true;
+        }
+
+        int steps = Mathf.Max(
+            Mathf.Abs(newPosition.x - oldPosition.x),
+            Mathf.Abs(newPosition.y - oldPosition.y)
+        );
+
+        for (int step = 1; step < steps; step++)
+        {
+            float t = step / (float)steps;
+            float angleOffset = (Mathf.PI / 2) * t;
+            float sampleAngle = startAngle + rotationDirection * angleOffset;
+
+            Vector2Int sampleCell = GridMath.GetGridPointOnCircle(sampleAngle, radius, playerCorePosition);
+
+            if (sampleCell == clusterPosition)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    #endregion
+
+    #region Cluster Movement
+
+    public static void MoveCluster(WorldCluster cluster, Vector2Int direction, int strength, List<WorldCluster> worldClusters)
+    {
+        
     }
 
     #endregion
